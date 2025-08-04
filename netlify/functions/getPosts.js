d01b32f50e28585186695e2397eaadd1902120fc
benjaminpockett <benjaminpockett@gmail.com>
2025-08-04 17:26:16 +0100

    Update getPosts.js

--- netlify/functions/getPosts.js
+++ netlify/functions/getPosts.js
@@ -1,12 +1,9 @@
-const fs = require("fs");
-const path = require("path");
-
-exports.handler = async () => {
-  const filePath = path.join(__dirname, "posts.json");
-  const posts = JSON.parse(fs.readFileSync(filePath, "utf-8"));
+export async function handler() {
+  const { kv } = await import("@netlify/kv");
+  const posts = (await kv.get("posts")) || [];
   return {
     statusCode: 200,
     body: JSON.stringify(posts),
     headers: { "Content-Type": "application/json" },
   };
-};
+}
