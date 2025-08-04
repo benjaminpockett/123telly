607262b2f4acda04ffc929a34c9df99f354f4e15
benjaminpockett <benjaminpockett@gmail.com>
2025-08-04 19:18:50 +0100

    Delete netlify/functions directory

--- netlify/functions/getPosts.js
+++
@@ -1,9 +0,0 @@
-export async function handler() {
-  const { kv } = await import("@netlify/kv");
-  const posts = (await kv.get("posts")) || [];
-  return {
-    statusCode: 200,
-    body: JSON.stringify(posts),
-    headers: { "Content-Type": "application/json" },
-  };
-}
