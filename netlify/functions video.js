1c121c0874d1ff507eb000d1fb5cbb978c276c24
benjaminpockett <benjaminpockett@gmail.com>
2025-08-05 16:20:55 +0100

    Update and rename netlify/functions/stream.js to netlify/functions video.js

--- netlify/functions video.js
+++ netlify/functions video.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 exports.handler = async (event) => {
   const { authorization } = event.headers;
-  const VALID_PASSWORD = 'secret123'; // Use environment variable in real apps
+  const VALID_PASSWORD = 'secret123'; // Change to use env var in production
 
   if (authorization !== `Bearer ${VALID_PASSWORD}`) {
     return {
@@ -12,15 +12,8 @@ exports.handler = async (event) => {
     };
   }
 
-  const filename = event.queryStringParameters?.file;
-  if (!filename || !/^[\w\-\.]+\.ts$/.test(filename)) {
-    return {
-      statusCode: 400,
-      body: 'Invalid file name',
-    };
-  }
+  const filePath = path.join(__dirname, '../../private-ts/myvideo.mp4');
 
-  const filePath = path.join(__dirname, '../../private-ts/', filename);
   if (!fs.existsSync(filePath)) {
     return {
       statusCode: 404,
@@ -29,10 +22,11 @@ exports.handler = async (event) => {
   }
 
   const fileBuffer = fs.readFileSync(filePath);
+
   return {
     statusCode: 200,
     headers: {
-      'Content-Type': 'video/MP2T',
+      'Content-Type': 'video/mp4',
       'Cache-Control': 'no-store',
     },
     body: fileBuffer.toString('base64'),
